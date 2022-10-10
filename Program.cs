ac331215826e342ab797eaac63eb5883fa525894
Dino Ribeiro <DRibeiro1124@gmail.com>
2022-10-10 18:29:16 -0400

    lots of help but got a working game

--- Program.cs
+++ Program.cs
@@ -15,20 +15,12 @@ namespace BlackJackCS
     class Deck
     {
         public List<Card> deckOfCards = new List<Card>();
+
         public Deck CreateDeck()
         {
+            List<string> suits = new List<string>() { "♠️", "♥️", "♦️", "♣️" };
+            List<string> ranks = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             Deck newDeck = new Deck();
-            List<string> suits = new List<string>()
-            {
-              "♠️", "♥️", "♦️", "♣️"
-            };
-
-            List<string> ranks = new List<string>()
-            {
-              "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
-            };
-
-            var deck = new List<Card>();
 
             foreach (var suit in suits)
             {
@@ -36,19 +28,19 @@ namespace BlackJackCS
                 {
                     var cardValue = 0;
 
-                    if (rank == "J")
+                    if (rank == "Jack")
                     {
                         cardValue = 10;
                     }
-                    else if (rank == "Q")
+                    else if (rank == "Queen")
                     {
                         cardValue = 10;
                     }
-                    else if (rank == "K")
+                    else if (rank == "King")
                     {
                         cardValue = 10;
                     }
-                    else if (rank == "A")
+                    else if (rank == "Ace")
                     {
                         cardValue = 11;
                     }
@@ -58,12 +50,13 @@ namespace BlackJackCS
                     }
 
                     Card singleCard = new Card { Suit = suit, Rank = rank, Point = cardValue };
-                    deck.Add(singleCard);
+                    newDeck.deckOfCards.Add(singleCard);
                 }
             }
 
             return newDeck;
         }
+
 
         public void Shuffle()
         {
@@ -109,7 +102,7 @@ namespace BlackJackCS
         {
             foreach (Card card in this.deckOfCards)
             {
-                Console.Write(card.Point + " of " + card.Suit + ", ");
+                Console.Write(card.Rank + " of " + card.Suit + "  ");
             }
             Console.WriteLine();
         }
@@ -117,16 +110,20 @@ namespace BlackJackCS
 
     class GameGreeting
     {
-        static void Greeting()
+        public void Greeting()
         {
             Console.WriteLine("------------------------------------");
+            Console.WriteLine();
             Console.WriteLine("Welcome to BlackJack!");
             Console.WriteLine(" ♠️ ♥️ ♦️ ♣️  ♠️ ♥️ ♦️ ♣️  ♠️ ♥️ ♦️ ♣️  ♠️ ♥️ ♦️ ♣️ ");
             Console.WriteLine("------------------------------------");
             Console.WriteLine();
             Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("Enter any key to play");
             var pressToPlay = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 
@@ -136,9 +133,192 @@ namespace BlackJackCS
     {
         static void Main(string[] args)
         {
-            var gameStart = new GameGreeting();
-            Console.WriteLine(gameStart);
 
+            GameGreeting greet = new GameGreeting();
+            greet.Greeting();
+
+            Boolean gameStart = true;
+            Boolean playAgain = false;
+            while (gameStart)
+            {
+                Deck newDeck = new Deck();
+                newDeck = newDeck.CreateDeck();
+                newDeck.Shuffle();
+
+                Hand playerHand = new Hand();
+                Hand dealerHand = new Hand();
+                playerHand.AddCard(newDeck.Deal());
+                playerHand.AddCard(newDeck.Deal());
+                dealerHand.AddCard(newDeck.Deal());
+                dealerHand.AddCard(newDeck.Deal());
+
+                Console.WriteLine($"You are holding: ");
+                playerHand.ShowHand();
+
+                if (playerHand.HandValue() > 21)
+                {
+                    Console.WriteLine($"Oh no you bust! ☹️ ☹️ ☹️");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($"Would you like to play another game of Blackjack?");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    var playerResponse = Console.ReadLine().ToLower();
+                    if (playerResponse == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+                playAgain = false;
+                Boolean gamePlay = true;
+                while (gamePlay)
+                {
+                    Console.WriteLine($"Type \"Hit\" to hit or \"Stand\" to stand");
+                    var hitOrStand = Console.ReadLine().ToLower();
+
+                    if (hitOrStand == "hit")
+                    {
+                        playerHand.AddCard(newDeck.Deal());
+                        Console.WriteLine($"Your hand now is: ");
+                        playerHand.ShowHand();
+
+                        if (playerHand.HandValue() > 21)
+                        {
+                            Console.WriteLine($"Oh no you bust! ☹️ ☹️ ☹️");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine($"Would you like to try again? Type Y to play again or N to quit playing");
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            var playerResponse = Console.ReadLine().ToLower();
+
+                            if (playerResponse == "y")
+                            {
+                                playAgain = true;
+                                break;
+                            }
+                            else
+                            {
+                                return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (playAgain)
+                {
+                    continue;
+                }
+                playAgain = false;
+
+                Boolean dealerGamePlay = true;
+                while (dealerGamePlay)
+                {
+                    Console.WriteLine($"The dealer's hand value is: ");
+                    dealerHand.ShowHand();
+                    if (dealerHand.HandValue() > 21)
+                    {
+                        Console.WriteLine($"Yes! Dealer busted and you win! 😎 😎 😎");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine($"Would you like to play another game of Blackjack? Type Y to play again or N to quit playing");
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        var playerResponse = Console.ReadLine().ToLower();
+
+                        if (playerResponse == "y")
+                        {
+                            playAgain = true;
+                            break;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                    else if (dealerHand.HandValue() >= 17)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dealerHand.AddCard(newDeck.Deal());
+                    }
+                }
+
+                if (playAgain)
+                {
+                    continue;
+                }
+                playAgain = false;
+
+                if (playerHand.HandValue() > dealerHand.HandValue())
+                {
+                    Console.WriteLine($"Your hand is better than dealers, You Win! 😎 😎 😎");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($"Would you like to play another game of Blackjack? Type Y to play again or N to quit playing");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    var playerResponse = Console.ReadLine().ToLower();
+
+                    if (playerResponse == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else if (playerHand.HandValue() < dealerHand.HandValue())
+                {
+                    Console.WriteLine($"Oh no! Your hand is lower than the dealers hand and you lose ☹️ ☹️ ☹️ ");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($"Would you like to play another game of Blackjack? Type Y to play again or N to quit playing");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    var playerResponse = Console.ReadLine().ToLower();
+
+                    if (playerResponse == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Your hand has the same value as the dealer hand...You lose! ☹️ ☹️ ☹️");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine($"Would you like to play another game of Blackjack? Type Y to play again or N to quit playing");
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    var playerResponse = Console.ReadLine().ToLower();
+
+                    if (playerResponse == "y")
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
         }
     }
 }
